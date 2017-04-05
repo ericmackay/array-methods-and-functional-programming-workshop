a0c671275c69141060637fe9273290cfefb1e8de
Eric MacKay <eric.mackay@gmail.com>
2017-04-05 19:11:17 +0000

    added solutions to forEach & map

--- workshop.js
+++ workshop.js
@@ -1,17 +1,15 @@
 function forEach(callback, theArray) {
-  /*
-  This is a little hint to get you started ;)
-  
-  for (... theArray.length ...) {
-    ...
-    callback(...)
-    ...
+  for (var i = 0; i < theArray.length; i++) {
+    callback(theArray[i]);
   }
-  */
 }
 
 function map(mappingFunction, theArray) {
-
+  var newMap=[];
+  for (var i = 0; i < theArray.length; i++) {
+    newMap.push(mappingFunction(theArray[i]));
+  }
+  return newMap;
 }
 
 function filter(predicate, theArray) {
